c32f66d496282ce1fbf6219aa41814681a5440eb
Brendan Divney <41346679+bdivney@users.noreply.github.com>
2021-04-25 16:16:23 -0400

    passwordHash needed to be a bigger varchar

--- eventDB/creates/createUsers.sql
+++ eventDB/creates/createUsers.sql
@@ -2,7 +2,7 @@ create table users
 (username varchar(20),
  firstName varchar(20),
  lastName varchar(20),
- passwordHash varchar(20),
+ passwordHash varchar(100),
  email varchar(320),
  CONSTRAINT usersPK PRIMARY KEY (username)
 );
