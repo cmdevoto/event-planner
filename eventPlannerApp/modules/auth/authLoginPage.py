0e7853a1f8b27aebd2df17246c9817cc66ed4306
Brendan Divney <bdivney@nd.edu>
2021-04-26 15:57:29 -0400

    login

--- eventPlannerApp/modules/auth/authLoginPage.py
+++ eventPlannerApp/modules/auth/authLoginPage.py
@@ -1,7 +1,23 @@
 from flask import render_template
 from werkzeug.security import check_password_hash
 from . import bp
+from ... import dbInterface
 
 @bp.route("/login")
 def hello_route():
     return render_template("auth/authLoginPage.html")
+
+
+@bp.route('/login', methods=['POST'])
+def createAccountSubmit():
+    email = request.form['email']
+    password = generate_password_hash(request.form['pass'], "sha256")
+    
+    searchQuery = "select PASSWORDHASH from users where email = :email"
+    searchParams = {
+        "email": email
+        }
+    result = dbInterface.fetchOne(searchQuery, searchParams)
+    print("Here is the password hash: " + result)
+
+    return redirect("home/homePage.html")
