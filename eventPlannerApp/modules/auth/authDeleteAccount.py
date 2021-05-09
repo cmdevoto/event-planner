8baac25c21c2bf457282f83854e708551c84c667
Brendan Divney <bdivney@nd.edu>
2021-05-09 14:49:26 -0400

    1

--- eventPlannerApp/modules/auth/authDeleteAccount.py
+++ eventPlannerApp/modules/auth/authDeleteAccount.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, flash
-from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
 from . import bp
 from ... import dbInterface, User
 from flask_login import current_user, login_user, LoginManager, login_required, logout_user
