d12c0cb17fe77bac7d6a465d759d61b288866dd9
Christian Amstutz <camstut1@nd.edu>
2021-05-12 12:01:02 -0400

    fixed single event view method hopefully

--- eventPlannerApp/modules/events/viewEventPage.py
+++ eventPlannerApp/modules/events/viewEventPage.py
@@ -1,9 +1,10 @@
 from flask import redirect, render_template
+from flask_login import login_required
 
 from . import bp
 from ... import dbInterface
 
-
+@login_required
 @bp.route("/event/<int:postId>")
-def single_event_view_page():
-  return ''
+def single_event_view_page(postId):
+    return 'Looking for postId: {}'.format(postId)
