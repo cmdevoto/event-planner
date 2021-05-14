eaddf3d829bc9b8a493f5e8241173eeb6fe10809
Christian Amstutz <camstut1@nd.edu>
2021-05-14 12:43:20 -0400

    month issues

--- eventPlannerApp/modules/events/editEventPage.py
+++ eventPlannerApp/modules/events/editEventPage.py
@@ -71,7 +71,7 @@ class EditEventForm(FlaskForm):
     month = SelectField('Month',
         choices=[(1, calendar.month_name[1]), (2, calendar.month_name[2]), (3, calendar.month_name[3]), 
                   (4, calendar.month_name[4]), (5, calendar.month_name[5]), (6, calendar.month_name[6]), 
-                  (7, calendar.month_name[7]),(8, calendar.month_name[8]), (9, calendar.month_name[9]), 
+                  (7, calendar.month_name[7]), (8, calendar.month_name[8]), (9, calendar.month_name[9]), 
                   (10, calendar.month_name[10]), (11, calendar.month_name[11]), (12, calendar.month_name[12])],
         validators=[DataRequired()]
         )
