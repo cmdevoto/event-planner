54b6601925f2df8d9e6f221f833c9e7518b2f002
Brendan Divney <bdivney@nd.edu>
2021-05-16 23:24:14 -0400

    finished i think

--- eventPlannerApp/modules/events/createPost.py
+++ eventPlannerApp/modules/events/createPost.py
@@ -1,5 +1,4 @@
 from flask import render_template, request, redirect, flash
-from werkzeug.security import generate_password_hash
 from . import bp
 from ... import dbInterface, User
 from flask_login import current_user, login_user, LoginManager, login_required, logout_user
