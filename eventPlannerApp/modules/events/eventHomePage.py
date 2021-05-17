d29d9c0cff49ea9514f8f659cbc17ff8b52bea38
Brendan Divney <bdivney@nd.edu>
2021-05-16 22:27:47 -0400

    maybe upcoming

--- eventPlannerApp/modules/events/eventHomePage.py
+++ eventPlannerApp/modules/events/eventHomePage.py
@@ -1,15 +1,16 @@
 from flask import redirect, render_template
 from flask_login import current_user, login_user, LoginManager, login_required, logout_user
-
+from datetime import date
 from . import bp
 from ... import dbInterface
 
 @bp.route("/events")
 @login_required
 def eventHomePageRoute():
-
+    today = date.today()
+    formattedToday = today.strftime("%d-%b-%y")
     #resultingEvents = dbInterface.fetchAll("select * from events", {})
-    resultingEvents = dbInterface.fetchAll("select * from events where eventID in (select eventID from eventInvitations where inviteeUsername = (:iid) and status = 'Accepted') or ownerUsername = (:iid) or creatorUsername = (:iid)", {"iid" : current_user.get_id()})
+    resultingEvents = dbInterface.fetchAll("select * from events where eventtime > (:today) and (eventID in (select eventID from eventInvitations where inviteeUsername = (:iid) and status = 'Accepted') or ownerUsername = (:iid) or creatorUsername = (:iid))", {"iid" : current_user.get_id(), "today" : formattedToday})
 
 
     data = {
