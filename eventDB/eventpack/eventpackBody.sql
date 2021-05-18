d4c31650f438949a24f6824df2a4d3278f661f72
Christian Amstutz <camstut1@nd.edu>
2021-05-18 17:01:29 -0400

    tyring to get eventpack working

--- eventDB/eventpack/eventpackBody.sql
+++ eventDB/eventpack/eventpackBody.sql
@@ -1,4 +1,4 @@
-create or replace package eventpack 
+create or replace package body eventpack 
 as
   function checkValidAccess(username user.username%type, eventId events.eventID%type)
     return varchar2
