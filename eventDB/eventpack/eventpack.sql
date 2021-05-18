d510003b9b11a2059087f86f82dea89002b56359
Christian Amstutz <camstut1@nd.edu>
2021-05-18 17:06:58 -0400

    tyring to get eventpack working 3

--- eventDB/eventpack/eventpack.sql
+++ eventDB/eventpack/eventpack.sql
@@ -1,6 +1,6 @@
 create or replace package eventpack 
 as
-  function checkValidAccess(username user.username%type, eventId events.eventID%type)
+  function checkValidAccess(username users.username%type, eventId events.eventID%type)
     return varchar2;
 end eventpack;
 /
