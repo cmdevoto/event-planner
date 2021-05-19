d7d270ef490e8ca947a8ab1663389af785bdc317
Christian Amstutz <camstut1@nd.edu>
2021-05-18 22:20:55 -0400

    fixed hopefully

--- eventPlannerApp/modules/invitations/createInvitationPage.py
+++ eventPlannerApp/modules/invitations/createInvitationPage.py
@@ -90,7 +90,8 @@ def createInvitationSubmit():
         for t in tempQuery:
             inviteSet.add(t[0])
         ownerQuery = dbInterface.fetchOne("select ownerUsername from groups where ownerUsername = (:ownerUsername)", {"ownerUsername": owner})
-        inviteSet.add(ownerQuery[0])
+        if ownerQuery[0] != owner:
+            inviteSet.add(ownerQuery[0])
 
     if current_user.get_id() != owner:
         inviteSet.add(owner)
